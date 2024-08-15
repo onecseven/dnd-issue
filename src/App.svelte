1886f427556fa0a15e3c91853a09e35b5fc5ed9f
Tati <iced.tati@gmail.com>
2024-08-15 17:23:54 -0400

    adding steps

--- src/App.svelte
+++ src/App.svelte
@@ -9,32 +9,46 @@
 </script>
 
 <main>
-  <section class="callout"
-  use:dndzone="{{ items: $store, flipDurationMs}}"
-  on:consider="{handleSort}"
-  on:finalize="{handleSort}"
-  >
+  <section>
+    <h1>Reproductions steps</h1>
+    <h1>1. Open two tabs of this page</h1>
+    <h1>2. Change the order of the boxes</h1>
+    <h1>3. Whichever tab was not active now has errored out (see dev tools) and will not work right</h1>
+  </section>
+    <section class="callout"
+    use:dndzone="{{ items: $store, flipDurationMs}}"
+    on:consider="{handleSort}"
+    on:finalize="{handleSort}"
+    >
   {#each $store as item(item.id)}
     <div class="container">{item.val}</div>
   {/each}
   </section>
+ 
 </main>
 
 <style>
+    main {
+      display:flex;
+    }
+
+    h1{
+      font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
     .callout {
     background-color: rgba(63, 62, 62, 0.365);
     border: 1px solid rgba(211, 211, 211, 0.386);
     border: 0;
     box-shadow: none;
     margin: 0;
-    margin-left: 0px;
-    margin-left: 30%;
+    margin-left: 10%;
     padding:0;
     width: auto;
     max-width: 20vw;
     float: left;
     min-width: 210px;
   }
+
   .callout > .container {
     margin: 30px;
   }
